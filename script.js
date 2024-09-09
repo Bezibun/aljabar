41e6af08fe1deb875a5aa8bd918f8477989d797b
Bezibun <fauzanputra@gmail.com>
2024-09-09 10:13:43 +0700

    first

---
+++ script.js
@@ -0,0 +1,22 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.page');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    sections.forEach(section => {
+        section.classList.add('hidden');
+        observer.observe(section);
+    });
+});
